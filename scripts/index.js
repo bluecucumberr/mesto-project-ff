0e2c2aa4ce54a7e59d5a4b139f114ee0ae738cca
Ekaterina <solovyovaalw@gmail.com>
2024-08-07 20:18:16 +0300

    1) исправлено имя переменной с cardList на cardsContainer; 2) для контентного изображения заполнен alt; 3) функция лайка задана аналогично функции удаления; 4) исправлен параметр функции удаления;

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,31 @@
 import { initialCards } from './cards.js';
 
 const cardTemplate = document.querySelector('#card-template').content;
-const cardList = document.querySelector('.places__list');
+const cardsContainer = document.querySelector('.places__list');
 
-function createCard (cardData, deleteCard) {
+function createCard (cardData, deleteCard, likeCard) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImg =  cardElement.querySelector('.card__image');
 
-    cardElement.querySelector('.card__image').src = cardData.link; 
+    cardImg.src = cardData.link; 
     cardElement.querySelector('.card__title').textContent = cardData.name;
+    cardElement.querySelector('.card__image').alt = cardData.name;
 
     cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard); 
-
-    cardElement.querySelector('.card__like-button').addEventListener('click', function (evt) {
-        evt.target.classList.toggle('card__like-button_is-active');
-    }); 
+    cardElement.querySelector('.card__like-button').addEventListener('click', likeCard);
     
     return cardElement;
 }
 
 initialCards.forEach(function(item) {
-    const cardElement = createCard(item, deleteCard);
-    cardList.append(cardElement);
+    const cardElement = createCard(item, deleteCard, likeCard);
+    cardsContainer.append(cardElement);
 })
 
-function deleteCard(element) {
-    element.target.closest('.card').remove(); 
+function deleteCard(evt) {
+    evt.target.closest('.card').remove(); 
+}
+
+function likeCard(evt) {
+    evt.target.classList.toggle('card__like-button_is-active');
 }
